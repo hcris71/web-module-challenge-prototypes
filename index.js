75b7216df3e247ed964987aa0ce66c0da8f35d5d
Cristopher Hernandez <hcris71@hotmail.com>
2022-10-03 17:51:51 -0700

    completed the prototypes challenge

--- index.js
+++ index.js
@@ -15,10 +15,22 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name,age) {
+this.name = name;
+this.age = age;
+this.stomach = [];
 }
-
+Person.prototype.eat = function(edible){
+if (this.stomach.length < 10){
+this.stomach.push(edible);
+}
+}
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
 
 /*
   TASK 2
@@ -36,10 +48,25 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model,milesPerGallon) {
+this.model = model;
+this.milesPerGallon = milesPerGallon;
+this.tank = 0;
+this.odometer = 0;
 }
-
+Car.prototype.fill = function(gallons){
+this.tank = this.tank + gallons;
+}
+Car.prototype.drive = function (distance){
+const driveableMiles = this.tank + gallons;
+if (distance <= driveableMiles){
+  this.odometer = this.odometer + distance;
+this.tank = this.tank - (distance/milesPerGallon);
+}else {this.odometer = this.odometer + driveableMiles;
+this.tank = 0;
+return `I ran out of fuel at ${this.odometer} miles!`
+}
+}
 
 /*
   TASK 3
@@ -49,18 +76,23 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+Person.call(this,name,age);
+this.favoriteToy = favoriteToy;
+}
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
 }
 
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window binding, the first, is the kind that binds to the window directly. This refers to an error as it can be trailed back to a lack of context within the code.
+  2. implicit binding is brought on by using .syntax and is commonly used to bind items to functions.
+  3. explicit binding uses .call. apply, .bind to bind a function to the .syntax used. 
+  4. new binding creates a new object altogether as a constructor function. 
 */
 
 ///////// END OF CHALLENGE /////////
